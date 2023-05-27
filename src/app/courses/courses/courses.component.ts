e7180ef93f3b1fa395e2a416b8339d7340a0859b
paulo.almeida <paulo.almeida.terceirizado@ana.gov.br>
2023-05-27 19:46:10 -0300

    Created a toolbar and mat-table to list courses

---
+++ src/app/courses/courses/courses.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss']
+})
+export class CoursesComponent implements OnInit{
+
+
+  courses: Course[] = [
+    {id: '1', name: 'Angular', category: 'front-end'  }
+    ];
+
+
+  displayedColumns = ['id', 'name', 'category']
+
+  constructor(){
+  }
+
+
+  ngOnInit(): void {
+
+  }
+}
